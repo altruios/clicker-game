5facbf81ba3d5c6f5a6e96756c664fe990b53d72
altruios <49083558+altruios@users.noreply.github.com>
2020-06-07 10:20:29 -0700

    Updates

---
+++ precache-manifest.d31214373e56fbc70b5a9a0e4de2cf4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c913981fd787ebd400d5d72cb9396e15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b1d2667b73df789dda4",
+    "url": "/static/css/main.8f5a486c.chunk.css"
+  },
+  {
+    "revision": "92b2b2afcbc71a1b8c12",
+    "url": "/static/js/2.f2e46a07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f2e46a07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b1d2667b73df789dda4",
+    "url": "/static/js/main.c63519f5.chunk.js"
+  },
+  {
+    "revision": "f9928d6c619321a1b2d8",
+    "url": "/static/js/runtime-main.82e7cd88.js"
+  }
+]);
